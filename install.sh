1e8dae9d09b29003300101093f4d4e7111fadd70
Piotr Koper <piotr.koper@gmail.com>
2012-06-02 02:25:52 +0200

    $gtmdir/$gtmver/r path

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Example installation procedure
 # See http://tinco.pair.com/bhaskar/gtm/doc/books/pg/UNIX_manual/ch11s04.html#examples_fo_using_ext_calls for details
 
-cp pcre.m pcreexamples.m ~/.fis-gtm/V5.5-000_x86/r/
+cp pcre.m pcreexamples.m $gtmdir/$gtmver/r/
 cp libpcre.so pcre.xc pcre.env $gtm_dist/plugin/
 sed -i "s|gtm_dist|$gtm_dist|" $gtm_dist/plugin/pcre.xc $gtm_dist/plugin/pcre.env
 echo ". $gtm_dist/plugin/pcre.env" >> ~/.profile
